c5e89c86279cc19178ccd3228d610bac0ab61a07
JP <jpteixeiraferreira@gmail.com>
2023-03-08 16:36:56 -0300

    Test

--- bin/projeto_inicial.dart
+++ bin/projeto_inicial.dart
@@ -1,8 +1,17 @@
 void main(){
   int cpf_cliente = 17616641741;
   bool cpf_valido = func_validar_cpf(cpf_cliente);
+  double saldo = 100.00;
   func_mostrar_cpf_valido(cpf_valido);
+  func_acrescenta_juros(701, saldo, nome_cliente: 'João Pedro', juros: 10);
+}
 
+double func_acrescenta_juros(int dias, double saldo , {String nome_cliente = 'não identificado', double juros = 0.05}){
+  saldo = saldo + (saldo * (juros/30)) * dias;
+  print('O saldo do cliente $nome_cliente após $dias dias é de R\$ ${saldo.toStringAsFixed(2)}');
+  print('O saldo do cliente $nome_cliente após $dias dias é de R\$ $saldo');
+
+  return saldo;
 }
 bool func_validar_cpf(int cpf){
   if(cpf.toString().length == 11){
